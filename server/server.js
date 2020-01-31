3cfe37ad8fc7e7adee31d5bc12dd0975587285c5
Drakx116 <alexandre2.guyot@gmail.com>
2020-01-31 12:56:40 +0100

    Add default API routes and body-parser

--- server/server.js
+++ server/server.js
@@ -3,6 +3,18 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.route('/')
+    .get((req, res) => {
+        res.status(200).json({
+            message : "I'm alive."
+        })
+    });
+
 app.listen(port,
     () => console.log(`Server is running on localhost:${port}`)
 );
